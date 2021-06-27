ef8c22affd39676aaa1c2e47bbc7688f4a16334d
banerjeesoumya15 <35263147+banerjeesoumya15@users.noreply.github.com>
2021-06-27 22:47:07 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'test_package'
+AUTHOR = 'Soumya'
+AUTHOR_EMAIL = 'banerjeesoumya15@gmail.com.com'
+URL = 'https://github.com/banerjeesoumya15/test_package'
+
+#LICENSE = 'Apache License 2.0'
+LICENSE = 'MIT License'
+DESCRIPTION = 'Describe your package in one sentence'
+#LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESCRIPTION = "First package"
+LONG_DESC_TYPE = "text"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
